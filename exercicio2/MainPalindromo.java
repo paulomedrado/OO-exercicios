6f2a8d41c3bb78f109bee5824b45b1931dbc7caa
paulomedrado <110437888+paulomedrado@users.noreply.github.com>
2022-11-24 19:29:45 -0300

    Exercicio2 - Palindromo

---
+++ exercicio2/MainPalindromo.java
@@ -0,0 +1,16 @@
+package exercicio2;
+
+public class MainPalindromo {
+
+	public static void main(String[] args) {
+		Palindromo P1;
+		
+		P1 = new Palindromo("Socorram me em Marrocos");
+		System.out.println(P1.contarVogais());
+		System.out.println(P1.inverso());
+		System.out.println(P1.resultado());
+		
+
+	}
+
+}
